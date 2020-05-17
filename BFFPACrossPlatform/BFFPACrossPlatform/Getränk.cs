ff234638cf1b6c41b7d29eca3933f8f10bce1f6a
MLit <inf3246@hs-worms.de>
2020-05-17 17:03:46 +0200

    User und Getränkklasse added

---
+++ BFFPACrossPlatform/BFFPACrossPlatform/Getränk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFFPACrossPlatform
+{
+    class Getränk
+    {
+        int ml;
+        int vol;
+        static double alcweight = 0.8;
+        public double calcalc;
+
+        public Getränk(int ml, int vol)
+        {
+            this.ml = ml;
+            this.vol = vol;
+            calcalc = ml * (vol / 100) * alcweight;
+        }
+
+    }
+}
--- BFFPACrossPlatform/BFFPACrossPlatform/User.cs
+++ BFFPACrossPlatform/BFFPACrossPlatform/User.cs
@@ -6,6 +6,50 @@ namespace BFFPACrossPlatform
 {
     class User
     {
+        double weight;
+        bool gender;
+        static double akfm = 0.68;
+        static double akfw = 0.55;
+        String name;
+        double bak;
+        Getränk[] getränke;
+        double bakUG;
+        double bakOG;
+        bool currentState;
 
+
+        public User(double weight, String name, Boolean gender, double bakUG, double bakOG)
+        {
+            this.weight = weight;
+            this.gender = gender;
+            this.name = name;
+            this.bakUG = bakUG;
+            this.bakOG = bakOG;
+        }
+
+        public void addDrink(Getränk getränk)
+        {
+            getränke[getränke.Length] = getränk;
+
+        }
+
+        public void calcBAK(Getränk getränk)
+        {
+            bak += getränk.calcalc;
+           currentState = checkBAK(bak, bakUG, bakOG);
+        }
+
+        public bool checkBAK(double bak, double bakUG, double bakOG)
+        {
+            if(bak < bakOG && bak > bakUG)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
     }
 }
